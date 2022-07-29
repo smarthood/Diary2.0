56a34866f797cbd8959098ce51ca8b98386e28fc
smarthood <renolddickson18@gmail.com>
2022-07-29 20:59:25 +0530

    added notebook style

--- src/components/Left.js
+++ src/components/Left.js
@@ -1,23 +1,23 @@
-import React ,{useState} from 'react'
-import profile from '../images/boy.jpeg' 
+import React ,{useState,useEffect} from 'react'
 import '../styles/App.css'
-import Right from './Right'
-import { Link } from 'react-router-dom'
 import { Box } from '@mui/system'
 import { AppBar, Button, ButtonGroup, IconButton, Stack, Toolbar, Typography } from '@mui/material'
 import { PhotoCamera } from '@mui/icons-material'
 import {Timestamp} from 'firebase/firestore';
 export default function Left() {
+
   const today = new Date()
   var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
   const handleClear=()=>{
+    document.getElementById("frm").reset()
+    document.getElementById('text-box').value = '';
     setFormData("")
   }
   const [formData,setFormData]=useState(
     {
       title: "",
       desc: "",
-      image: "",
+      image: "null",
       createdAt: Timestamp.now().toDate()
     }
   )
@@ -25,7 +25,8 @@ export default function Left() {
     setFormData({...formData,[e.target.name]:e.target.value})
   }
   const handleImageChange=(e)=>{
-    setFormData({...formData,image:e.target.file[0]});
+    setSelectedImage(e.target.file[0])
+    setFormData({...formData,image:selectedImage});
   }
   const handleOnSubmit=()=>{
       if(!formData.title||!formData.desc||!formData.image){
@@ -33,18 +34,27 @@ export default function Left() {
         return;
       }
   }
+   const [imageUrl, setImageUrl] = useState(null);
+  const [selectedImage,setSelectedImage]=useState(null)
+  useEffect(() => {
+    if (selectedImage) {
+      setImageUrl(URL.createObjectURL(selectedImage));
+    }
+  }, [selectedImage]);
+  console.log(imageUrl)
   return (
     <Box bgcolor="dodgerblue" minHeight="100vh">
       <Stack direction="row" justifyContent="space-between">
     <Box flex={8}>
-    <AppBar sx={{color:"white",zIndex:"0"}} color="transparent" position="static" elevation="0" >
+    <AppBar sx={{color:"white",zIndex:"0"}} color="transparent" position="fixed" elevation="0" >
         <Toolbar sx={{ displey: 'flex',
           flexDirection: 'row',
           justifyContent: 'space-between'}}>
         <Typography variant='h5' sx={{fontFamily: 'Anton',fontSize: '30px'}}>TODAY'S ADVENTURE</Typography>
-        <Typography sx={{marginRight:{sm: "5%"}}}>{date}</Typography>
+        <Typography sx={{marginRight:{sm: "5px",color: "black"}}}>{date}</Typography>
         </Toolbar>
       </AppBar>
+      <Toolbar />
       <Box 
       id="frm"
       component="form" sx={{
@@ -52,6 +62,18 @@ export default function Left() {
         display: 'flex',
         flexDirection: "column",
       }}>
+        <Box sx={{display:"flex",justifyContent:"center",marginBottom: "20px"}}>
+    <Button variant="contained" sx={{backgroundColor: "white",color: "black",'&:hover': {
+      backgroundColor: 'white',
+      color: 'black',
+  }}} component="label" >
+  Upload
+  <input hidden accept="image/*" name="image" onChange={(e)=>handleImageChange(e)} type="file" />
+</Button>
+<IconButton color="primary" aria-label="upload picture" component="label">
+  <PhotoCamera  sx={{color: "white"}}/>
+</IconButton>
+      </Box>
      <input type="text" className="input" name="title" placeholder="Title here....." value={formData.title} onChange={(e)=>handleOnChange(e)}/>
       <textarea  id="text-box" cols="30" name="desc" placeholder="How is your day today?" rows="10" value={formData.desc} onChange={(e)=>handleOnChange(e)}></textarea>
       <ButtonGroup variant="container" sx={{marginTop:"20px"}}>
@@ -66,19 +88,30 @@ export default function Left() {
   </ButtonGroup>
       </Box>
     </Box>
-    <Box bgcolor="white" minHeight="100vh" p={3} flex={3} sx={{ boxShadow: 3,display: { xs: 'none', sm: 'block' }}}>
-      <Box sx={{display:"flex",justifyContent:"center"}}>
-    <Button variant="outlined" component="label" >
-  Upload
-  <input hidden accept="image/*" name="image" onChange={(e)=>handleImageChange(e)} type="file" />
-</Button>
-<IconButton color="primary" aria-label="upload picture" component="label">
-  <input hidden accept="image/*" type="file" />
-  <PhotoCamera />
-</IconButton>
-      </Box>
+    <Box id="text_wrap" maxWidth="500px"  minHeight="100vh" p={3} flex={4} sx={{ boxShadow: 3,display: { xs: 'none', sm: 'block' }}}>
+      <div className="lines">
+
+      <Box component="div" sx={{display: "flex",justifyContent:"center"}}>
+      <div class="img-tape img-tape--2">
+    <Box
+        component="img"
+        sx={{
+          height: 233,
+          width: 250,
+          maxHeight: {  md: 167 },
+          maxWidth: { md: 250 },
+          border: "5px solid white"
+        }}
+        alt="The house from the offer."
+        src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
+        />
+        </div>
+        </Box>
+      <Box sx={{marginLeft:"15%"}}>
     <Typography sx={{fontFamily: 'Edu VIC WA NT Beginner',fontSize:"30px",textAlign:"center",textTransform:"capitalize"}}>{formData.title}</Typography>
-    <Typography sx={{fontFamily:"Shadows Into Light",fontSize:"20px",textTransform:"fir"}}>{formData.desc}</Typography>
+    <Typography sx={{fontFamily:"Shadows Into Light",fontSize:"20px",marginTop:"10px"}}>{formData.desc}</Typography>
+      </Box>
+        </div>
     </Box>
       </Stack>
     </Box>
